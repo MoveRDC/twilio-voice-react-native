d9d903873fb3c53c9d8fc3b6ccbc3fa275c449d5
Thomas Verry <thomas.verry@realtor.com>
2025-08-04 14:21:50 -0500

    fix: review feedback

--- android/src/main/java/com/twiliovoicereactnative/ExpoActivityLifecycleListeners.java
+++ android/src/main/java/com/twiliovoicereactnative/ExpoActivityLifecycleListeners.java
@@ -6,50 +6,50 @@ import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.widget.Toast;
+
 import expo.modules.core.interfaces.ReactActivityLifecycleListener;
 
 public class ExpoActivityLifecycleListeners implements ReactActivityLifecycleListener {
-  private VoiceActivityProxy voiceActivityProxy;
+    private VoiceActivityProxy voiceActivityProxy;
 
-  @Override
-  public void onCreate(Activity activity, Bundle savedInstanceState) {
-    this.voiceActivityProxy = new VoiceActivityProxy(activity, permission -> {
-      if (Manifest.permission.RECORD_AUDIO.equals(permission)) {
-        Toast.makeText(
-          activity,
-          "Microphone permissions needed. Please allow in your application settings.",
-          Toast.LENGTH_LONG).show();
-      } else if ((Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) &&
-        Manifest.permission.BLUETOOTH_CONNECT.equals(permission)) {
-        Toast.makeText(
-          activity,
-          "Bluetooth permissions needed. Please allow in your application settings.",
-          Toast.LENGTH_LONG).show();
-      } else if ((Build.VERSION.SDK_INT > Build.VERSION_CODES.S_V2) &&
-        Manifest.permission.POST_NOTIFICATIONS.equals(permission)) {
-        Toast.makeText(
-          activity,
-          "Notification permissions needed. Please allow in your application settings.",
-          Toast.LENGTH_LONG).show();
-      }
-    });
-    this.voiceActivityProxy.onCreate(savedInstanceState);
-  }
-
-  @Override
-  public boolean onNewIntent(Intent intent) {
-    // do we need this check?
-    if (this.voiceActivityProxy != null) {
-      this.voiceActivityProxy.onNewIntent(intent);
+    @Override
+    public void onCreate(Activity activity, Bundle savedInstanceState) {
+        this.voiceActivityProxy = new VoiceActivityProxy(activity, permission -> {
+            if (Manifest.permission.RECORD_AUDIO.equals(permission)) {
+                Toast.makeText(
+                        activity,
+                        "Microphone permissions needed. Please allow in your application settings.",
+                        Toast.LENGTH_LONG).show();
+            } else if ((Build.VERSION.SDK_INT >= Build.VERSION_CODES.S)
+                    && Manifest.permission.BLUETOOTH_CONNECT.equals(permission)) {
+                Toast.makeText(
+                        activity,
+                        "Bluetooth permissions needed. Please allow in your application settings.",
+                        Toast.LENGTH_LONG).show();
+            } else if ((Build.VERSION.SDK_INT > Build.VERSION_CODES.S_V2)
+                    && Manifest.permission.POST_NOTIFICATIONS.equals(permission)) {
+                Toast.makeText(
+                        activity,
+                        "Notification permissions needed. Please allow in your application settings.",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+        this.voiceActivityProxy.onCreate(savedInstanceState);
     }
 
-    return false;
-  }
+    @Override
+    public boolean onNewIntent(Intent intent) {
+        if (this.voiceActivityProxy != null) {
+            this.voiceActivityProxy.onNewIntent(intent);
+        }
 
-  @Override
-  public void onDestroy(Activity activity) {
-    if (this.voiceActivityProxy != null) {
-      this.voiceActivityProxy.onDestroy();
+        return false;
     }
-  }
+
+    @Override
+    public void onDestroy(Activity activity) {
+        if (this.voiceActivityProxy != null) {
+            this.voiceActivityProxy.onDestroy();
+        }
+    }
 }
--- android/src/main/java/com/twiliovoicereactnative/TwilioVoiceReactNativeModule.java
+++ android/src/main/java/com/twiliovoicereactnative/TwilioVoiceReactNativeModule.java
@@ -459,35 +459,6 @@ public class TwilioVoiceReactNativeModule extends ReactContextBaseJavaModule {
     });
   }
 
-  // Register/UnRegister
-
-
-
-//  @ReactMethod void voice_handleEvent(ReadableMap messageData, Promise promise) {
-//    logger.debug(".voice_handleEvent()");
-//
-//    mainHandler.post(() -> {
-//      logger.debug(".voice_handleEvent() > runnable");
-//
-//      // parse data to string map
-//      final HashMap<String, String> parsedMessageData = new HashMap<>();
-//      ReadableMapKeySetIterator iterator = messageData.keySetIterator();
-//      while (iterator.hasNextKey()) {
-//        String key = iterator.nextKey();
-//        parsedMessageData.put(key, messageData.getString(key));
-//      }
-//      // attempt to parse message
-//      if (Voice.handleMessage(
-//        reactContext,
-//        parsedMessageData,
-//        new CallMessageListenerProxy())) {
-//        promise.resolve(true);
-//      } else {
-//        promise.resolve(false);
-//      }
-//    });
-//  }
-
   // CallInvite
 
   @ReactMethod
